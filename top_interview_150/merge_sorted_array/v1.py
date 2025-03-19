e2ba0c7eca986bcb318db793951cff6ecf7e2779
Mikhail Barinov <megorov45@ya.ru>
2025-03-19 18:03:48 +0300

    Purfect! 1 is done

--- top_interview_150/merge_sorted_array/v1.py
+++ top_interview_150/merge_sorted_array/v1.py
@@ -6,12 +6,35 @@ class Solution:
         temp_list = []
         for i in range(m):
             temp_list.append(nums1[i])
-        print(temp_list)
-
-        # i = 0
-        # j = 0
-        # while
+        i = 0
+        j = 0
+        k = 0
+        while i < m or j < n:
+            if i >= m:
+                nums1[k] = nums2[j]
+                k += 1
+                j += 1
+            elif j >= n:
+                nums1[k] = temp_list[i]
+                i += 1
+                k += 1
+            else:
+                if temp_list[i] < nums2[j]:
+                    nums1[k] = temp_list[i]
+                    i += 1
+                    k += 1
+                elif temp_list[i] == nums2[j]:
+                    nums1[k] = temp_list[i]
+                    i += 1
+                    k += 1
+                    nums1[k] = nums2[j]
+                    k += 1
+                    j += 1
+                else:
+                    nums1[k] = nums2[j]
+                    k += 1
+                    j += 1
 
 
 solution = Solution()
-solution.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+solution.merge([1], 1, [], 0)
