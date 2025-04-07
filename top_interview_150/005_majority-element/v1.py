6b6f69a6b05ed88e7843c382d85c1b8f78b94ab0
Mikhail Barinov <mbarinov@yandex.ru>
2025-04-07 17:02:15 +0300

    005 Pyhon O(n^2) solution

---
+++ top_interview_150/005_majority-element/v1.py
@@ -0,0 +1,30 @@
+# This is O(n^2) solution
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dummy = 1000000001
+        counter = 0
+        i = 0
+        while i < len(nums):
+            if nums[i] != dummy:
+                current_element = nums[i]
+                j = i
+                while j < len(nums):
+                    if nums[j] == current_element:
+                        counter += 1
+                        nums[j] = dummy
+                    j += 1
+                if counter > len(nums) / 2:
+                    return current_element
+                i += 1
+            else:
+                i += 1
+
+
+solution = Solution()
+test_subject = [1000,2,1001,1000,1000]
+print(test_subject)
+print(solution.majorityElement(test_subject))
