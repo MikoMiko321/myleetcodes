7bad860c07bcab19acd34d2e51f357a29b2149e6
Mikhail Barinov <mbarinov@yandex.ru>
2025-08-13 01:52:41 +0300

    after a long break

---
+++ top_interview_150/017_roman-to-integer/v1.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        print(s)
+
+mysolution = Solution()
+mysolution.romanToInt("test")
